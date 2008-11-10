ef8909570653c98bcf9654dfd5d563c0f298b4a9
dberube <dberube@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-10 04:48:12 +0000

    First public version.
    git-svn-id: https://plugins.svn.wordpress.org/mofuse/trunk@73167 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mofuse.php
@@ -0,0 +1,151 @@
+<?php
+/*
+Plugin Name: MoFuse Plugin
+Plugin URI: http://www.mofuse.com
+Description: MoFuse's Wordpress plugin. If you need a MoFuse account, visit <a href="http://www.mofuse.com">www.mofuse.com</a> and create a free account and you can have a mobile version of your Wordpress blog up and running in just seconds.
+Author: MoFuse
+Version: 0.5.1 
+Author URI: http://www.mofuse.com
+*/
+
+// Get options from Wordpress Database
+	$mf_sid=get_option("mf_sid");
+	$mf_redirect=get_option("mf_redirect");
+	$mf_sms=get_option("mf_sms");
+	$mf_cname=get_option("mf_cname");
+	
+	$mf_subd=str_replace("http://", "", get_settings('home'));
+	$currenturl=$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+	if ($mf_subd[strlen($mf_subd)-1]=='/') { } else {
+		$mf_subd=$mf_subd.'/';
+	}
+
+// Mobile detect & redirect
+	
+	$mf_ua=urlencode($_SERVER["HTTP_USER_AGENT"]);
+	$mf_url="http://api.mofuse.com/?action=mobiledetect&useragent=$mf_ua";
+	if (@file_get_contents($mf_url)==1 && $_GET['mf_nr']!=1) {
+		$mf_ismobile=1;
+	}
+	
+	function mf_hpd($currenturl, $mf_subd) {
+		if($currenturl=="" || $currenturl=="/" || $currenturl==$mf_subd) {
+			return true;
+		} else {
+			return false; 
+		}
+	}
+
+	if ($mf_redirect=='y') {
+		if($mf_ismobile==1){ 
+			if (mf_hpd($currenturl, $mf_subd)) {
+				//header("Location: http://www.google.com");
+				add_action('wp_head', "sendtoMobile",3);
+			}
+		}
+	}
+
+	function sendtoMobile() {
+		global $mf_sid;
+		global $mf_cname;
+		if ($mf_cname) {
+			$mf_cname=str_replace("http://", "", $mf_cname);
+			header("Location: http://$mf_cname");
+		} else {
+			header("Location: http://$mf_sid.mofuse.mobi");
+		}
+	}
+
+// End of Mobile detect & redirection
+
+// Plugin work
+	add_action('admin_menu', 'mf_add_pages');
+
+	function mf_add_pages() {
+		add_options_page('MoFuse', 'MoFuse', 10, __FILE__, 'mf_toplevel_page');
+	}
+
+	function mf_toplevel_page() {
+		echo '<img src="http://mofuse.com/images/logo_small.png" style="margin:20px 0px 20px 20px;" />';
+	
+	    if($_POST['mf_hidden']==1) {
+			$mf_sid=$_POST['mf_sid'];
+			$mf_redirect=$_POST['mf_redirect'];
+			$mf_sms=$_POST['mf_sms'];
+			$mf_cname=$_POST['mf_cname'];
+        
+			update_option('mf_sid', $mf_sid);
+			update_option('mf_redirect', $mf_redirect);
+			update_option('mf_sms', $mf_sms);
+			update_option('mf_cname', $mf_cname);
+			
+			?>
+			<div class="updated"><p><strong><?php _e('Options saved.', 'mt_trans_domain' ); ?></strong></p></div>
+			<?php
+		}
+
+// Get options from Wordpress Database (again)
+	$mf_sid=get_option("mf_sid");
+	$mf_redirect=get_option("mf_redirect");
+	$mf_sms=get_option("mf_sms");
+	$mf_cname=get_option("mf_cname");
+
+// Show stuff on the MoFuse Options page
+    echo '<div class="wrap">';
+//    echo "<h2>" . __( 'MoFuse Plugin Options', 'mf_trans_domain' ) . "</h2>";
+	echo '<p style="margin-top: 10px; padding: 10px; background-color: #ffffec; font-size: 17px; border: 1px solid #ebebeb; color: #333;">
+	<img src="http://snapple.mofuse.com/users/images/icons/exclamation.png" align="absmiddle" style="margin-right: 5px;" /> 
+	If you don\'t already have a MoFuse account you can visit <a href="http://www.mofuse.com" target="_blank">www.mofuse.com</a> and create one for free.</p>';    
+    ?>
+
+	<form name="form1" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+		<input type="hidden" name="mf_hidden" value="1">
+
+		<div style="margin: 0 0 20px 0; padding: 10px; border: 1px solid #012D7E; background-color: #fbfbfb;">
+			<p style="font-size: 17px; color: #333333; margin: 0 0 0 0;"><?php _e("Your MoFuse Site ID:", 'mf_trans_domain' ); ?></p>
+			<p style="margin: 5px 0 0 10px;">
+	        http://<input name="mf_sid" type="text" size="20" value="<?php echo $mf_sid; ?>">.mofuse.mobi
+			<br /><span style="color: #666666;">This is in your MoFuse URL, for example http://myblog.mofuse.mobi -- <b>myblog</b> would be your Site ID.</span>
+			</p>
+
+			<p style="font-size: 17px; color: #333333; margin: 20px 0 0 0;"><?php _e("Custom Domain:", 'mf_trans_domain' ); ?></p>
+			<p style="margin: 5px 0 0 10px;">
+	        http://<input name="mf_cname" type="text" size="40" value="<?php echo $mf_cname; ?>">
+			<br /><span style="color: #666666;">This feature only works if you have properly setup your custom domain name.</span>
+			</p>
+            
+        </div>
+
+		<div style="margin: 0 0 20px 0; padding: 10px; border: 1px solid #012D7E; background-color: #fbfbfb;">
+	        <p style="font-size: 17px; color: #333333; margin: 0 0 0 0;"><?php _e("Enable Automatic Detect & Redirect:", 'mf_trans_domain' ); ?></p>
+			<p style="margin: 5px 0 0 10px;">
+			<label>
+			<input name="mf_redirect" type="radio" id="mf_redirect_0" value="y" <?php if ($mf_redirect=='y') { echo 'checked="checked"'; } ?> />
+			Yes</label>
+			<label>
+			<input type="radio" name="mf_redirect" value="n" id="mf_redirect_1" <?php if ($mf_redirect=='n') { echo 'checked="checked"'; } ?> />
+			No</label>
+		</p>
+
+		<p style="font-size: 17px; color: #999999; margin: 20px 0 0 0; display: none;"><?php _e("Enable SMS Widget:", 'mf_trans_domain' ); ?></p>
+			<p style="margin: 5px 0 0 10px; display: none;">
+    	    <label>
+			<input name="mf_sms" type="radio" id="mf_sms_0" value="y" <?php if ($mf_sms=='y') { echo 'checked="checked"'; } ?> />
+			Yes</label>
+			<label>
+			<input type="radio" name="mf_sms" value="n" id="mf_sms_1" <?php if ($mf_sms=='n') { echo 'checked="checked"'; } ?> />
+			No</label>
+    	    <br /><span style="color: #999999;">Not active in this version</span>
+			</p>
+        </div>
+
+		<p class="submit">
+		<input type="submit" name="Submit" value="<?php _e('Update Options', 'mf_trans_domain' ) ?>" />
+		</p>
+
+	</form>
+	</div>
+
+<?php
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== MoFuse Wordpress Plugin ===
+Contributors: dberube
+Tags: mobile, mobilephone, cellphone, mofuse, xhtml, wap, handset, web
+Requires at least 2.0.2
+Test up to: 2.6.3
+Stable tag: 2.6
+
+This plugin works with a MoFuse account.  This plugin can automatically detect and redirect mobile visitors to the mobile-friendly version of this blog.
+
+=== Description ===
+
+MoFuse (http://www.mofuse.com) is a service that allows bloggers to easily create a mobile-friendly version of their blog for free.
+
+This plugin allows you to automatically detect and redirect your mobile visitors to your mobile-friendly version.
+
+=== Installation ===
+
+You can install the MoFuse plugin by unpacking the distributed plugin file in the wp-content/plugins directory of your WordPress installation.
+
+Once unpacked you should see a "MoFuse Plugin" entry in the "Plugins" tab of your administrative interface for WordPress. You'll need to use the "Activate" link under "Plugin Management" to start using this plugin.
+
+You will need a MoFuse account in order to use this plugin. Once you have a MoFuse account you can click on the "MoFuse Options" tab of your administrative interface to enter in your MoFuse mobile site details.
+
+From the "MoFuse Options" tab you must enter in your MoFuse Site ID. Your MoFuse Site ID is the subdomain you are using on MoFuse. For example, if the URL to your MoFuse mobile site is http://myblog.mofuse.mobi then your MoFuse Site ID is: myblog.
+
+From the "MoFuse Options" tab you can tell the MoFuse plugin whether or not you want to automatically detect & redirect mobile visitors to your blog to your MoFuse mobile site.
+
+=== Frequently Asked Questions ===
+
+= How do I use this plugin if I don't have a MoFuse account? =
+
+You can't use this plugin without a MoFuse account.
+
+= How can I get a MoFuse account? =
+
+You can create a free MoFuse account by visiting http://www.mofuse.com.
+
+= How long does it take to create a mobile site? =
+
+It takes just seconds to create a mobile-friendly version of your blog using MoFuse.
+ 
